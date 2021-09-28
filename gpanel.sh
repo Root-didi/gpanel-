2b77db7980c16e2b117732da6535b54094a0913d
Root-didi <73555229+Root-didi@users.noreply.github.com>
2021-09-28 19:26:07 +0100

    Update gpanel.sh

--- gpanel.sh
+++ gpanel.sh
@@ -5,7 +5,7 @@ curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 apt -y install nodejs make gcc g++
 mkdir -p /srv/daemon /srv/daemon-data
 cd /srv/daemon
-curl -L https://github.com/pterodactyl/daemon/releases/download/v0.6.13/daemon.tar.gz | tar --strip-components=1 -xzv
+curl -L https://github.com/Root-didi/daemon/archive/refs/tags/ff.zip | tar --strip-components=1 -xzv
 npm install --only=production --no-audit --unsafe-perm
 nano /srv/daemon/config/core.json
 npm start
